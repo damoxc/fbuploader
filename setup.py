ad6a4bb71fe0ff631b75e97c74cfcc7e36b66eba
Damien Churchill <damoxc@gmail.com>
2009-03-04 20:29:00 +0000

    change version number to 0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@
 from setuptools import setup, find_packages
 
 setup(name="FBUploader",
-      version="0.0.1",
+      version="0.1",
       license="GPLv3",
       description="Facebook photo uploader",
       long_description="""""",
