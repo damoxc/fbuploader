09fa54258e5d935fbc2be3bbeb4d2f4908d61d70
Damien Churchill <damoxc@gmail.com>
2009-03-04 15:35:57 +0000

    create a threaded photo uploader, still needs some work

--- fbuploader/upload_dialog.py
+++ fbuploader/upload_dialog.py
@@ -20,6 +20,7 @@
 # 	Boston, MA    02110-1301, USA.
 #
 
+import os
 import gtk
 import logging
 import threading
@@ -27,35 +28,68 @@ from fbuploader.common import Dialog, signal
 
 log = logging.getLogger(__name__)
 
+class PhotoUploader(threading.Thread):
+
+    def __init__(self, facebook, aid, upload_cb, tag_cb):
+        super(PhotoUploader, self).__init__()
+        self.upload_cb = upload_cb
+        self.tag_cb = tag_cb
+        self.queue = []
+        self.aid = aid
+        self.do_upload = facebook.photos.upload
+        self.add_tag = facebook.photos.addTag
+    
+    def run(self):
+        while 1:
+            if not self.queue: break
+            photo, info = self.queue.pop(0)
+            log.info("Uploading photo: %s", os.path.basename(photo))
+            upload_info = self.do_upload(photo, self.aid, info.get("caption"))
+            self.upload_cb(photo)
+            pid = upload_info["pid"]
+            def format_tag(tag):
+                tag_dict = {"x": tag[1], "y": tag[2]}
+                if type(tag[0]) is int:
+                    tag_dict["tag_uid"] = tag[0]
+                else:
+                    tag_dict["tag_text"] = tag[0]
+            tags = map(format_tag, info.get("tags", []))
+            log.info("Tagging photo: %s", photo)
+            if tags:
+                self.add_tag(pid, tags=tags)
+            self.tag_cb(photo)
+    
+    def upload(self, photo, info):
+        self.queue.append((photo, info))
+
 class UploadDialog(Dialog):
     
     def __init__(self, main_window):
         super(UploadDialog, self).__init__("upload_dialog")
         log.info("Initializing upload dialog.")
         self.main_window = main_window
-        self.do_upload = self.main_window.facebook.photos.upload
-        self.add_tag = self.main_window.facebook.photos.addTag
-        self.aid = self.main_window.album["aid"]
+        self.total_progressbar = self.tree.get_widget("upload_total_progressbar")
+        self.current_progressbar = self.tree.get_widget("upload_current_progressbar")
     
     def run(self):
+        self.dialog.show()
+        aid = self.main_window.album["aid"]
+        facebook = self.main_window.facebook
+        
+        uploader = PhotoUploader(facebook, aid, self.on_upload, self.on_tag)
+        self.total_photos = len(self.main_window.photos)
+        self.complete_photos = 0
         for photo in self.main_window.photos:
-            self.upload_photo(photo)
+            uploader.upload(photo, self.main_window.photo_info[photo])
+        uploader.start()
+        
+    def on_upload(self, photo):
+        self.complete_photos += 1
+        progress = (self.complete_photos / float(self.total_photos))
+        self.total_progressbar.set_fraction(progress)
     
-    def upload_photo(self, photo):
-        info = self.main_window.photo_info[photo]
-        log.info("Uploading photo: %s", photo)
-        upload_info = self.do_upload(photo, self.aid, info.get("caption"))
-        pid = upload_info["pid"]
-        def format_tag(tag):
-            tag_dict = {"x": tag[1], "y": tag[2]}
-            if type(tag[0]) is int:
-                tag_dict["tag_uid"] = tag[0]
-            else:
-                tag_dict["tag_text"] = tag[0]
-        tags = map(format_tag, info.get("tags", []))
-        log.info("Tagging photo: %s", photo)
-        if tags:
-            self.add_tag(pid, tags=tags)
+    def on_tag(self, photo):
+        pass
 
     @signal
     def on_upload_dialog_delete_event(self, *args):
